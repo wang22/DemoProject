f9f6e7b2fcaf852ccb124c8ac04b359c6b84f0d5
小王王 <pause22@163.com>
2020-08-29 22:14:41 +0800

    增加元素

---
+++ tiledmap/tsx/spike-head.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="spike-head" tilewidth="1" tileheight="1" tilecount="2808" columns="54">
+ <image source="../assets/spike-head.png" width="54" height="52"/>
+</tileset>
