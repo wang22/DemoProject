858961d04656e6a052707154fb8b8710d3ce13c5
小王王 <pause22@163.com>
2020-08-30 16:00:27 +0800

    设计关卡

---
+++ tiledmap/tsx/flag.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="flag" tilewidth="1" tileheight="1" tilecount="4096" columns="64">
+ <image source="../assets/flag.png" width="64" height="64"/>
+</tileset>
